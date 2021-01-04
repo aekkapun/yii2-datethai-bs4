d135eb04aeb031a9fa5480c2a97cf17aee247459
aekkapun sriwong <aekkapun@users.noreply.github.com>
2021-01-04 23:03:41 +0700

    Update DatePickerLanguageAsset.php

--- src/DatePickerLanguageAsset.php
+++ src/DatePickerLanguageAsset.php
@@ -3,7 +3,7 @@
  * @copyright Copyright (c) 2018 Akaranan Naree
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
-namespace karatae99\datepicker;
+namespace aekkapun\datepicker;
 
 use yii\web\AssetBundle;
 
@@ -18,6 +18,6 @@ class DatePickerLanguageAsset extends AssetBundle
     public $sourcePath = '@bower/bootstrap-datepicker/dist/locales';
 
     public $depends = [
-        'karatae99\datepicker\DateRangePickerAsset'
+        'aekkapun\datepicker\DateRangePickerAsset'
     ];
 }
